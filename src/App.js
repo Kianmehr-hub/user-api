711ba91fb68bbede9b1a9f720f882b36f2955bec
Kianmehr-hub <Kianmehr90sh@outlook.com>
2022-08-24 11:04:22 +0430

    third commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
-import data from '../src/data/data.json'
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-
-import './App.css'
-import SinglePage from './components/SinglePage';
+import data from "../src/data/data.json";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const App = () => {
   const [list, setList] = useState([]);
 
   useEffect(() => {
-    setList(data)
-    console.log(list)
-  }, [list])
+    setList(data);
+    console.log(list);
+  }, [list]);
 
-  return(
-    <div className='spa'>
-      <table>
+  return (
+    <div className="container">
+      <table className="table">
         <tr>
           <th>Name</th>
           <th>Username</th>
           <th>Email</th>
           <th>Address</th>
         </tr>
-        {list.map((data) => 
-        <tr>
-          <td><Link to={`/${data.id}`} className='name'>{data.name}</Link></td>
-          <td className='username'>{data.username}</td>
-          <td className='email'>{data.email}</td>
-          <td className='address'>{data.address.city}</td>
-        </tr>
-        )}
+        {list.map((data) => (
+          <tr>
+            <td>
+              <Link to={`/${data.id}`} className="table-success">
+                {data.name}
+              </Link>
+            </td>
+            <td className="table-success">{data.username}</td>
+            <td className="email">{data.email}</td>
+            <td className="address">{data.address.city}</td>
+          </tr>
+        ))}
       </table>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/SinglePage.jsx
+++ src/components/SinglePage.jsx
@@ -1,18 +1,41 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { Link } from 'react-router-dom';
-import data from '../data/data.json'
+import { Link } from "react-router-dom";
+import data from "../data/data.json";
 
 const SinglePage = () => {
   const { dataId } = useParams();
-  const user = data.find(item => item.id === dataId)
-  console.log(data);
-  console.log(user)
+  const [selectedUser, setSelectedUser] = useState(null);
+  useEffect(() => {
+    setSelectedUser(data.find((item) => item.id == dataId));
+    console.log(selectedUser);
+  }, []);
 
   return (
-    <>
-      <p>{dataId}</p>
-      <Link to="/"><button>back</button></Link>
-    </>
+    <div className="container">
+      <div>
+        <button className="btn btn-warning m-1">
+          <Link to="/" className="text-decoration-none text-dark">back</Link>
+        </button>
+      </div>
+
+      <table className="table table-dark">
+        <tr>
+          <th className="h-name">name</th>
+          <th className="h-username">username</th>
+          <th className="h-email">email</th>
+          <th className="h-address">address</th>
+        </tr>
+        {selectedUser && (
+          <tr>
+            <td>{selectedUser.name}</td>
+            <td>{selectedUser.username}</td>
+            <td>{selectedUser.email}</td>
+            <td>{selectedUser.address.city}</td>
+          </tr>
+        )}
+      </table>
+    </div>
   );
 };
 
